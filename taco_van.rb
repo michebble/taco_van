90691f6d8cc8d59ae197d31d9ed53f1fa18bc3d5
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2023-11-06 13:04:04 +0900

    Udpate formatting

--- taco_van.rb
+++ taco_van.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 taco_count = ARGV.first&.to_i || 5
-names = File.readlines("data/names.txt").map(&:chomp).uniq
-words = File.readlines("data/adjectives.txt").map(&:chomp).uniq
-emojis = File.readlines("data/emojis.txt").map(&:chomp).uniq
+names = File.readlines('data/names.txt').map(&:chomp).uniq
+words = File.readlines('data/adjectives.txt').map(&:chomp).uniq
+emojis = File.readlines('data/emojis.txt').map(&:chomp).uniq
 
 my_words = words.sample(taco_count)
 my_picks = names.sample(taco_count)
 my_emojis = emojis.sample(taco_count)
 
-puts "✨Spreading positive vibes with 🌮 power!✨"
+puts '✨Spreading positive vibes with 🌮 power!✨'
 puts "\n"
 my_picks.each.with_index do |pick, index|
   puts "#{my_emojis[index]}  @#{pick} for being #{my_words[index]}  #{my_emojis[index]}"
